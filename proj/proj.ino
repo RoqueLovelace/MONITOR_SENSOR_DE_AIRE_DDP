d1068fa076b6e8c26ef9ce67834bb6a52bb4b9b5
RoqueLovelace <chrisdesanfrancisco@gmail.com>
2024-09-14 20:32:03 -0600

    Add Official Alpha version of ESP8266 Module code. Everything is working fine. Currently working with local ip and 5 seconds interval between request, that will be changed when finishing the part of reception and visualization of the data

--- proj/proj.ino
+++ proj/proj.ino
@@ -3,108 +3,260 @@
 #include <ESP8266HTTPClient.h>
 #include <LiquidCrystal_I2C.h>
 
-#define ledRojo 14
+#define ledVerde 14
 #define ledAmarillo 12
-#define ledVerde 13
+#define ledRojo 13
 #define sensorData A0
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Dirección I2C
+#pragma region VARIABLES DE CONTADOR
+unsigned long previousMillis = 0; // Guarda el último valor de millis()
+const int intervaloEntreRequests = 5;
+long intervaloContador = 1000;               // Intervalo de tiempo (1 segundo = 1000 ms)
+int tiempoRestante = intervaloEntreRequests; // Tiempo del temporizador en segundos (10 minutos = 600 segundos)
+#pragma endregion
 
-const char* ntpServer = "pool.ntp.org";  // Servidor NTP
-const long  gmtOffset_sec = -21600;      // Ajuste de zona horaria (para GMT-6, hora local)
-const int   daylightOffset_sec = 0;      // Ajuste por horario de verano
-int lastRequest = 0;
+#pragma region VARIABLES HOTA CON SERVER NTP
+const char *ntpServer = "pool.ntp.org"; // Servidor NTP
+const long gmtOffset_sec = -21600;      // Ajuste de zona horaria (para GMT-6, hora local)
+const int daylightOffset_sec = 0;       // Ajuste por horario de verano
+#pragma endregion
 
-//para ingresar a la red
-const char* ssid = "";
-const char* password = "";
+#pragma region WIFI
+const char *ssid = ";";
+const char *password = "";
+#pragma endregion
 
-String serverURL = "http://ip:1234/post"; 
+#pragma region LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Dirección I2C
+int charPosicion = 0;
+#pragma endregion
 
-void setup() {
+#pragma region SERVER
+String serverURL = "http://ipipip:1234/post";
+#pragma endregion
 
-  // inicializamos lcd
-  lcd.init();
-  lcd.backlight();  
-  lcd.setCursor(0, 0);
+#pragma region VARIABLES DE HTTPRequest
+int ppm = -1; // se escoge -1 por si hay algun fallo, se podra ver este valor irreal
+String jsonPayload = "";
+char timeStringBuff[50] = "0-0-0";
+int httpResponseCode = -1;
+String response = "";
+bool lastRequestFailed4Connection = false;
+bool lastRequestFailed = false;
+char tiempoFormato[6]; // Buffer para almacenar el tiempo formateado
+#pragma endregion
 
-  // inicializamos leds como output
-  pinMode(ledRojo, OUTPUT);  
+#pragma region STRINGS
+// tiempo, LastReqSuccesfull,  response, responseStatus
+// si no hay conexion: CHECK CONNECTION (4)
+String processOutput[4] = {"", "", "", ""};
+int contadorProcessOutput = 0;
+#pragma endregion
+
+void setup()
+{
+
+#pragma region Inicializar LEDs como salida
+  pinMode(ledRojo, OUTPUT);
   pinMode(ledAmarillo, OUTPUT);
   pinMode(ledVerde, OUTPUT);
+#pragma endregion
+  digitalWrite(ledVerde, HIGH);
+  digitalWrite(ledAmarillo, LOW);
+  digitalWrite(ledRojo, LOW);
+  delay(500);
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledAmarillo, HIGH);
+  digitalWrite(ledRojo, LOW);
+  delay(500);
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledAmarillo, LOW);
+  digitalWrite(ledRojo, HIGH);
+  delay(500);
 
-  //Inicializamos WIFI
+#pragma region inicializamos lcd
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+#pragma endregion
+
+#pragma region Inicializamos WIFI
   WiFi.begin(ssid, password);
   lcd.print("Conectando... ");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
+#pragma endregion
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledAmarillo, LOW);
+    digitalWrite(ledRojo, LOW);
+    delay(166);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarillo, HIGH);
+    digitalWrite(ledRojo, LOW);
+    delay(166);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarillo, LOW);
+    digitalWrite(ledRojo, HIGH);
+    delay(166);
     lcd.print(".");
   }
 
   lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("conectado, ip:");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 0);
+  lcd.print("CONECTADO, IP:");
+  lcd.setCursor(0, 1);
   lcd.print(WiFi.localIP());
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); // configuramos NTP
   delay(5000);
 
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);  // configuramos NTP
+  ppm = analogRead(sensorData); // se vincula el ppm leido
+  EnviarRequest();
 }
 
-void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
+void loop()
+{
+  // se obtiene el tiempo actual
+  unsigned long currentMillis = millis();
+
+  // se verifica si ha pasado 1 segundo
+  if (currentMillis - previousMillis >= intervaloContador)
+  {
+    previousMillis = currentMillis; // Guardar el tiempo actual para la próxima comparación
+
+    if (tiempoRestante > 0)
+    {
+
+      // se resta un segundo al temporizador
+      tiempoRestante--;
+
+      // se limpia la pantalla
+      lcd.clear();
+
+#pragma region IMPRIMIR PRIMER LINEA
+      // en este caso, tiene que irse mostrando informacion
+      // arriba ppm y tiempo
+      // 0123456789012345
+      // ppm:00000--00:00
+      //----------------
+
+      // Mostrar los ultimos ppm registrados
+      lcd.setCursor(0, 0);
+      lcd.print("ppm:");
+      lcd.print(ppm);
+
+      // Mostrar el tiempo restante en el LCD
+      sprintf(tiempoFormato, "%02d:%02d", tiempoRestante / 60, tiempoRestante % 60); // se formatea el tiempo
+      lcd.setCursor(11, 0);
+      lcd.print(tiempoFormato);
+#pragma endregion
+
+#pragma region IMPRIMIR SEGUNDA LINEA
+      // abajo intercalando entre: si hay conexion, hora de la ultima peticion, response de la ultima, status code
+      lcd.setCursor(0, 1);
+      lcd.print(processOutput[contadorProcessOutput].substring(0, 16));
+      if (contadorProcessOutput == 3)
+        contadorProcessOutput = 0;
+      else
+        contadorProcessOutput++;
+#pragma endregion
+
+#pragma region LEDs
+      if (ppm == -1 || ppm >= 1000)
+      {
+        // PPM no válido o muy alto
+        digitalWrite(ledVerde, LOW);
+        digitalWrite(ledAmarillo, LOW);
+        digitalWrite(ledRojo, HIGH);
+      }
+      else if (ppm >= 400)
+      {
+        // PPM en rango crítico
+        digitalWrite(ledVerde, LOW);
+        digitalWrite(ledAmarillo, HIGH);
+        digitalWrite(ledRojo, LOW);
+      }
+      else
+      {
+        // PPM en rango normal
+        digitalWrite(ledVerde, HIGH);
+        digitalWrite(ledAmarillo, LOW);
+        digitalWrite(ledRojo, LOW);
+      }
+#pragma endregion
+    }
+    else
+    {                               // Cuando el tiempo llega a 0
+      ppm = analogRead(sensorData); // se vincula el ppm leido
+      EnviarRequest();
+      tiempoRestante = intervaloEntreRequests; // Reiniciar el temporizador
+    }
+  }
+}
+
+void EnviarRequest()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  { // si no se ha roto la conexion a internet
+#pragma region VARIABLES DE CONECTION
     HTTPClient http;
     WiFiClient client;
-    int ppm = analogRead(sensorData); 
+#pragma endregion
 
+#pragma region TIMESTAMP
     // obtenemos la hora fecha y hora actual con un servidor NTP
     time_t now;
     struct tm timeinfo;
     time(&now);
     localtime_r(&now, &timeinfo);
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%d %H:%M:%S", &timeinfo); // se vincula la hora formateada en el buffer timeStringBuff
+#pragma endregion
 
-    // Formateamos el timestamp en formato legible
-    char timeStringBuff[50];  // Buffer para almacenar la cadena de tiempo
-    strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%d %H:%M:%S", &timeinfo);
+#pragma region presentacion de datos
+    jsonPayload = "{\"ppm\": " + String(ppm) + ", \"timestamp\": \"" + String(timeStringBuff) + "\"}";
 
-    // Creamos el JSON con el valor del sensor y el timestamp formateado
-    String jsonPayload = "{\"ppm\": " + String(ppm) + ", \"timestamp\": \"" + String(timeStringBuff) + "\"}";
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%d-%m-%y %H:%M", &timeinfo); // modificamos la hora para ahorar espacio en el lcd
+#pragma endregion
 
-    // modificamos la hora para ahorar espacio en el lcd
-    strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%d %H:%M", &timeinfo);
-    String nextRequest; //se calculara cuanto tiempo queda para la siguiente request.
-
+#pragma region ARMADO Y ENVIO DE Request
     http.begin(client, serverURL);
     http.addHeader("Content-Type", "application/json");
 
-    int httpResponseCode = http.POST(jsonPayload);
+    httpResponseCode = http.POST(jsonPayload); // se hace el Request y se recupera el status para proceder
+#pragma endregion
 
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("ppm:");
-    lcd.print(ppm);
-    
-
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-
-      lcd.setCursor(0,1);
-      lcd.print(timeStringBuff);  // Mostrar el tiempo formateado en la pantalla
-      delay(1000);
-      lcd.setCursor(0,1);
-      lcd.print("Res.code: " + String(httpResponseCode));
-      delay(1000);
-    } else {
-      lcd.setCursor(0,1);
-      lcd.print("ErrorInHTTPRequest");
-      delay(1000);
+#pragma region PROCEDIMIENTO POSTERIOR A ENVIO DE Request
+    if (httpResponseCode > 0)
+    { // si el httpResponseCode se envio, necesitamos saber si fue correcto o no
+      response = http.getString();
+      // tiempo, LastReqSuccesfull,  response, responseStatus
+      processOutput[0] = "Req. SENDED";
+      processOutput[1] = timeStringBuff;
+      processOutput[2] = "Req. CODE: " + String(httpResponseCode);
+      processOutput[3] = "Req.INFO:" + response;
     }
-    http.end();
-  } else {
-    lcd.setCursor(0,1);
-    lcd.print("WiFi not connected");
-    delay(1000);
+    else
+    {
+      processOutput[0] = "ERROR IN REQUEST";
+      processOutput[1] = "REQ. FAILED";
+      processOutput[2] = "SERVER ERROR";
+      processOutput[3] = String("LAST REQ. ") + timeStringBuff;
+    }
+#pragma endregion
+  }
+  else
+  { // si la conexion se rompio
+    // TODO: EVALUAR SI SE DEBERIA DE BORRAR INFO DE DATOS SACADOS DEL REQUEST, YA QUE NO SON VALIDOS
+    // lastRequestFailed = true;
+    // lastRequestFailed4Connection = true;
+    lcd.print("NO CONNECTION");
+    lcd.setCursor(0, 2);
+    lcd.print("Request failed");
+    processOutput[0] = "CHECK CONNECTION";
+    processOutput[1] = "NO REQ. SENDED";
+    processOutput[2] = "CHECK CONNECTION";
+    processOutput[3] = String("LAST REQ. ") + timeStringBuff;
   }
 }
--- test.ino/test.ino.ino
+++ test.ino/test.ino.ino
@@ -3,117 +3,259 @@
 #include <ESP8266HTTPClient.h>
 #include <LiquidCrystal_I2C.h>
 
-#define ledVerde 14
-#define ledAmarillo 12
-#define ledRojo 13
-#define sensorData A0
+#pragma region VARIABLES DE CONTADOR
+unsigned long previousMillis = 0; // Guarda el último valor de millis()
+const int intervaloEntreRequests = 80;
+long intervaloContador = 1000;               // Intervalo de tiempo (1 segundo = 1000 ms)
+int tiempoRestante = intervaloEntreRequests; // Tiempo del temporizador en segundos (10 minutos = 600 segundos)
+#pragma endregion
 
-// region VARIABLES DE CONTADOR
-unsigned long previousMillis = 0;    // Guarda el último valor de millis()
-const int intervaloEntreRequests = 10;
-const long interval = 1000;          // Intervalo de tiempo (1 segundo = 1000 ms)
-int tiempoRestante = intervaloEntreRequests;            // Tiempo del temporizador en segundos (10 minutos = 600 segundos)
-// endregion
-
-// region VARIABLES HOTA CON SERVER NTP
-const char* ntpServer = "pool.ntp.org";  // Servidor NTP
-const long  gmtOffset_sec = -21600;      // Ajuste de zona horaria (para GMT-6, hora local)
-const int   daylightOffset_sec = 0;      // Ajuste por horario de verano
+#pragma region VARIABLES HOTA CON SERVER NTP
+const char *ntpServer = "pool.ntp.org"; // Servidor NTP
+const long gmtOffset_sec = -21600;      // Ajuste de zona horaria (para GMT-6, hora local)
+const int daylightOffset_sec = 0;       // Ajuste por horario de verano
 int lastRequest = 0;
-// endregion
+#pragma endregion
 
-// region WIFI 
-const char* ssid = "";
-const char* password = "";
-// endregion
+#pragma region WIFI
+const char *ssid = ";";
+const char *password = "";
+#pragma endregion
 
-// region LCD
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Dirección I2C
-// endregion
+#pragma region LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Dirección I2C
+int charPosicion = 0;
+#pragma endregion
 
-// region SERVER
-String serverURL = "http://ip:1234/post";
-// endregion
+#pragma region SERVER
+String serverURL = "http://ipipip:1234/post";
+#pragma endregion
 
-void setup() {
-  // Inicializar LEDs como salida
-  pinMode(ledRojo, OUTPUT);  
+#pragma region VARIABLES DE HTTPRequest
+int ppm = -1; // se escoge -1 por si hay algun fallo, se podra ver este valor irreal
+String jsonPayload = "";
+char timeStringBuff[50] = "0-0-0";
+int httpResponseCode = -1;
+String response = "";
+bool lastRequestFailed4Connection = false;
+bool lastRequestFailed = false;
+char tiempoFormato[6]; // Buffer para almacenar el tiempo formateado
+#pragma endregion
+
+#pragma region STRINGS
+// tiempo, LastReqSuccesfull,  response, responseStatus
+// si no hay conexion: CHECK CONNECTION (4)
+String processOutput[4] = {"", "", "", ""};
+int contadorProcessOutput = 0;
+#pragma endregion
+
+void setup()
+{
+
+#pragma region Inicializar LEDs como salida
+  pinMode(ledRojo, OUTPUT);
   pinMode(ledAmarillo, OUTPUT);
   pinMode(ledVerde, OUTPUT);
+#pragma endregion
 
-  // inicializamos lcd
+#pragma region inicializamos lcd
   lcd.init();
-  lcd.backlight();  
+  lcd.backlight();
   lcd.setCursor(0, 0);
+#pragma endregion
 
-  // Inicializar puerto serie
-  Serial.begin(9600);
+#pragma region Inicializamos WIFI
+  WiFi.begin(ssid, password);
+  lcd.print("Conectando... ");
+  lcd.setCursor(0, 1);
+#pragma endregion
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledAmarillo, LOW);
+    digitalWrite(ledRojo, LOW);
+    delay(166);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarillo, HIGH);
+    digitalWrite(ledRojo, LOW);
+    delay(166);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarillo, LOW);
+    digitalWrite(ledRojo, HIGH);
+    delay(166);
+    lcd.print(".");
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("CONECTADO, IP:");
+  lcd.setCursor(0, 1);
+  lcd.print(WiFi.localIP());
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); // configuramos NTP
+  delay(5000);
+
+  ppm = analogRead(sensorData); // se vincula el ppm leido
+  EnviarRequest();
 }
 
-void loop() {
-  // Obtener el tiempo actual
+void loop()
+{
+  // se obtiene el tiempo actual
   unsigned long currentMillis = millis();
 
-  // Verificar si ha pasado 1 segundo
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;  // Guardar el tiempo actual para la próxima comparación
-    
-    if (tiempoRestante > 0) {
-      // Restar un segundo al temporizador
+  // se verifica si ha pasado 1 segundo
+  if (currentMillis - previousMillis >= intervaloContador)
+  {
+    previousMillis = currentMillis; // Guardar el tiempo actual para la próxima comparación
+
+    if (tiempoRestante > 0)
+    {
+
+      // se resta un segundo al temporizador
       tiempoRestante--;
 
-      // Mostrar el tiempo restante en el LCD
+      // se limpia la pantalla
       lcd.clear();
+
+#pragma region IMPRIMIR PRIMER LINEA
+      // en este caso, tiene que irse mostrando informacion
+      // arriba ppm y tiempo
+      // 0123456789012345
+      // ppm:00000--00:00
+      //----------------
+
+      // Mostrar los ultimos ppm registrados
       lcd.setCursor(0, 0);
-      lcd.print("Tiempo: ");
-      lcd.print(tiempoRestante / 60);  // Minutos restantes
-      lcd.print(":");
-      lcd.print(tiempoRestante % 60);  // Segundos restantes
-      
-      // También mostrar el tiempo restante en el puerto serie
-      Serial.print("Tiempo restante: ");
-      Serial.print(tiempoRestante / 60);
-      Serial.print(" minutos, ");
-      Serial.print(tiempoRestante % 60);
-      Serial.println(" segundos");
-    } else {
-      // Cuando el tiempo llega a 0
-      Serial.println("¡El tiempo ha terminado!");
+      lcd.print("ppm:");
+      lcd.print(ppm);
 
-      // Acción adicional: encender LED rojo
-      digitalWrite(ledVerde, HIGH);
-      delay(1000); // Mantener LED encendido 1 segundo
-      digitalWrite(ledVerde, LOW);
-      digitalWrite(ledRojo, HIGH);
-      delay(1000); // Mantener LED encendido 1 segundo
-      digitalWrite(ledRojo, LOW);
-      digitalWrite(ledAmarillo, HIGH);
-      delay(1000); // Mantener LED encendido 1 segundo
-      digitalWrite(ledAmarillo, LOW);
+      // Mostrar el tiempo restante en el LCD
+      sprintf(tiempoFormato, "%02d:%02d", tiempoRestante / 60, tiempoRestante % 60); // se formatea el tiempo
+      lcd.setCursor(11, 0);
+      lcd.print(tiempoFormato);
+#pragma endregion
 
-      // Reiniciar el temporizador a 10 minutos (600 segundos)
-      tiempoRestante = intervaloEntreRequests;
+#pragma region IMPRIMIR SEGUNDA LINEA
+      // abajo intercalando entre: si hay conexion, hora de la ultima peticion, response de la ultima, status code
+      lcd.setCursor(0, 1);
+      lcd.print(processOutput[contadorProcessOutput].substring(0, 16));
+      if (contadorProcessOutput == 3)
+        contadorProcessOutput = 0;
+      else
+        contadorProcessOutput++;
+#pragma endregion
 
-      // También podrías realizar cualquier otra acción aquí, como sonar una alarma, etc.
+#pragma region LEDs
+      if (ppm == -1 || ppm >= 1000)
+      {
+        digitalWrite(ledVerde, LOW);
+        digitalWrite(ledAmarillo, LOW);
+        digitalWrite(ledRojo, HIGH);
+      }
+      else if (ppm >= 400 || ppm < 1000)
+      {
+        digitalWrite(ledVerde, LOW);
+        digitalWrite(ledAmarillo, HIGH);
+        digitalWrite(ledRojo, LOW);
+      }
+      else
+      {
+        digitalWrite(ledVerde, HIGH);
+        digitalWrite(ledAmarillo, LOW);
+        digitalWrite(ledRojo, LOW);
+      }
+#pragma endregion
+    }
+    else
+    {                               // Cuando el tiempo llega a 0
+      ppm = analogRead(sensorData); // se vincula el ppm leido
+      EnviarRequest();
+      tiempoRestante = intervaloEntreRequests; // Reiniciar el temporizador
     }
   }
 }
 
+void EnviarRequest()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  { // si no se ha roto la conexion a internet
+#pragma region VARIABLES DE CONECTION
+    HTTPClient http;
+    WiFiClient client;
+#pragma endregion
 
-void probarLeds() {
-  digitalWrite(ledRojo, HIGH);  // Turn the LED on (Note that LOW is the voltage level
+#pragma region TIMESTAMP
+    // obtenemos la hora fecha y hora actual con un servidor NTP
+    time_t now;
+    struct tm timeinfo;
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%Y-%m-%d %H:%M:%S", &timeinfo); // se vincula la hora formateada en el buffer timeStringBuff
+#pragma endregion
+
+#pragma region presentacion de datos
+    jsonPayload = "{\"ppm\": " + String(ppm) + ", \"timestamp\": \"" + String(timeStringBuff) + "\"}";
+
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%d-%m-%y %H:%M", &timeinfo); // modificamos la hora para ahorar espacio en el lcd
+#pragma endregion
+
+#pragma region ARMADO Y ENVIO DE Request
+    http.begin(client, serverURL);
+    http.addHeader("Content-Type", "application/json");
+
+    httpResponseCode = http.POST(jsonPayload); // se hace el Request y se recupera el status para proceder
+#pragma endregion
+
+#pragma region PROCEDIMIENTO POSTERIOR A ENVIO DE Request
+    if (httpResponseCode > 0)
+    { // si el httpResponseCode se envio, necesitamos saber si fue correcto o no
+      response = http.getString();
+      // tiempo, LastReqSuccesfull,  response, responseStatus
+      processOutput[0] = "Req. SENDED";
+      processOutput[1] = timeStringBuff;
+      processOutput[2] = "Req. CODE: " + String(httpResponseCode);
+      processOutput[3] = "Req.INFO:" + response;
+    }
+    else
+    {
+      processOutput[0] = "ERROR IN REQUEST";
+      processOutput[1] = "REQ. FAILED";
+      processOutput[2] = "SERVER ERROR";
+      processOutput[3] = String("LAST REQ. ") + timeStringBuff;
+    }
+#pragma endregion
+  }
+  else
+  { // si la conexion se rompio
+    // TODO: EVALUAR SI SE DEBERIA DE BORRAR INFO DE DATOS SACADOS DEL REQUEST, YA QUE NO SON VALIDOS
+    // lastRequestFailed = true;
+    // lastRequestFailed4Connection = true;
+    lcd.print("NO CONNECTION");
+    lcd.setCursor(0, 2);
+    lcd.print("Request failed");
+    processOutput[0] = "CHECK CONNECTION";
+    processOutput[1] = "NO REQ. SENDED";
+    processOutput[2] = "CHECK CONNECTION";
+    processOutput[3] = String("LAST REQ. ") + timeStringBuff;
+  }
+}
+
+void ProbarLeds()
+{
+  digitalWrite(ledRojo, HIGH); // Turn the LED on (Note that LOW is the voltage level
   digitalWrite(ledAmarillo, LOW);
   digitalWrite(ledVerde, LOW);
   delay(1000);
 
-  digitalWrite(ledRojo, LOW);  
+  digitalWrite(ledRojo, LOW);
   digitalWrite(ledAmarillo, HIGH);
   digitalWrite(ledVerde, LOW);
   delay(1000);
 
-  digitalWrite(ledRojo, LOW);  
+  digitalWrite(ledRojo, LOW);
   digitalWrite(ledAmarillo, LOW);
   digitalWrite(ledVerde, HIGH);
   delay(1000);
 }
-
